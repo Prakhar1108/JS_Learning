e8f33184a1e5aee3db44d4210bbe0c30ccb1fa43
Prakhar1108 <165126032+Prakhar1108@users.noreply.github.com>
2025-12-30 06:22:48 +0000

    Date

--- JS_online/test.js
+++ JS_online/test.js
@@ -1,29 +1,46 @@
-// console.log("hi")
+// // console.log("hi")
 
-// const hero = ["hi","hello", "Akash"]
+// // const hero = ["hi","hello", "Akash"]
 
-// let user = {
-//     name : "Prakhar",
-//     age : 23
-// }
+// // let user = {
+// //     name : "Prakhar",
+// //     age : 23
+// // }
 
-// let myFucn = function(){
-//     console.log("Prakhar")
-// }
+// // let myFucn = function(){
+// //     console.log("Prakhar")
+// // }
 
-// myFucn()
-// console.log(typeof(myFucn))
-
-
-// const gameName = "Vishal"
-// const name = new String("Satyam")
-// console.log(name.length)
-// console.log(gameName)
+// // myFucn()
+// // console.log(typeof(myFucn))
 
 
-console.log(Math.PI)
-console.log(Math.abs(-4))
+// // const gameName = "Vishal"
+// // const name = new String("Satyam")
+// // console.log(name.length)
+// // console.log(gameName)
 
-console.log(Math.round(4.6))
 
-console.log(Math.random(1,10))
+// console.log(Math.PI)
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6))
+
+// console.log(Math.random(1,10))
+
+
+// --------------------Date Object------------------
+
+let myDate = new Date();
+//console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString(), myDate.toLocaleString())
+
+let myNewDate = new Date(2025,12,30)
+console.log(myNewDate.toDateString())
+
+let dateTimestap = Date.now();
+console.log(dateTimestap);
+console.log(myNewDate.getTime())
+
+
