bd03f79b427110dfebe19c8687fb3d7f9299f91c
Prakhar1108 <165126032+Prakhar1108@users.noreply.github.com>
2025-12-30 05:48:10 +0000

    Datatypes_summery

--- JS_online/test.js
+++ JS_online/test.js
@@ -1,1 +1,29 @@
-console.log("hi")
+// console.log("hi")
+
+// const hero = ["hi","hello", "Akash"]
+
+// let user = {
+//     name : "Prakhar",
+//     age : 23
+// }
+
+// let myFucn = function(){
+//     console.log("Prakhar")
+// }
+
+// myFucn()
+// console.log(typeof(myFucn))
+
+
+// const gameName = "Vishal"
+// const name = new String("Satyam")
+// console.log(name.length)
+// console.log(gameName)
+
+
+console.log(Math.PI)
+console.log(Math.abs(-4))
+
+console.log(Math.round(4.6))
+
+console.log(Math.random(1,10))
